4bbca3f4f348f2dc36ab7243039890594ae3b21a
Afonso Lage <lage.afonso@gmail.com>
2019-08-19 06:15:58 -0300

    Added a define for natural light shift

--- light/voxel_light.h
+++ light/voxel_light.h
@@ -2,9 +2,10 @@
 #define VOXEL_LIGHT_H
 
 #define LIGHT_MAX_VALUE 15
-
 #define NATURAL_LIGHT_MASK 0xF0
+#define NATURAL_LIGHT_SHIFT 4
 #define ARTIFICIAL_LIGHT_MASK 0x0F
+
 
 #define LIGHT_DECREASE_POWER 1
 
@@ -26,7 +27,7 @@ static inline int get_art_light(int value) {
 }
 
 static inline int get_nat_light(int value) {
-    return (value & NATURAL_LIGHT_MASK) >> 4;
+    return (value & NATURAL_LIGHT_MASK) >> NATURAL_LIGHT_SHIFT;
 }
 
 static inline int set_art_light(int value, int new_value) {
@@ -37,7 +38,7 @@ static inline int set_art_light(int value, int new_value) {
 
 static inline int set_nat_light(int value, int new_value) {
     value &= ~NATURAL_LIGHT_MASK;
-    value |= (new_value & ARTIFICIAL_LIGHT_MASK) >> 4;
+    value |= (new_value & ARTIFICIAL_LIGHT_MASK) >> NATURAL_LIGHT_SHIFT;
     return value;
 }
 
